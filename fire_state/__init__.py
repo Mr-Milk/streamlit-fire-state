f85b913e599f4cb8134cf839a549b8dac7cb16dc
Mr-Milk <zym.zym1220@gmail.com>
2023-05-27 11:11:58 +0800

    add version and description

--- fire_state/__init__.py
+++ fire_state/__init__.py
@@ -1,4 +1,6 @@
-__version__ = "0.1.0"
+"""Persist state across multiple pages in streamlit."""
+
+__version__ = "0.1.2"
 
 from .state import create_store, get_store, set_store, \
     get_state, set_state, form_update
