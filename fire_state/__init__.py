75a7a08e75647ee7f24b713c0768efd532be8e71
Yimin Zheng <zym.zym1220@gmail.com>
2024-09-30 08:02:07 +0000

    Bump version

--- fire_state/__init__.py
+++ fire_state/__init__.py
@@ -1,6 +1,6 @@
 """Persist state across multiple pages in streamlit."""
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 from .state import create_store, get_store, set_store, \
     get_state, set_state, form_update
